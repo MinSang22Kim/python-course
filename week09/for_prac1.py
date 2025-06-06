bae858e31740a47f3aca1afd354552d5bdf934fd
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-06-06 14:54:15 +0900

    feat: 파이썬 9주차 실습 코드 오탈자 수정

--- week09/for_prac1.py
+++ week09/for_prac1.py
@@ -1,13 +1,15 @@
 # 0부터 2까지 1씩 증가하면 출력(0, 1, 2가 줄바꿈되어 출력)
 for i in range(0, 3, 1):
     print(i)
+print()
 
 # 증가값이 커도 처음에 한번은 무조건 출력됨(0이 출력)
 for i in range(0, 3, 999):
     print(i)
+print()
 
 # hap 변수 초기화 필요
 hap = 0
 for i in range(2, 10, 1):
-    hab = hap + i
+    hap = hap + i
 print(hap)
