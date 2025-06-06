f1453a68e5711a405f6bd435e50598600947d2e9
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-06-06 16:08:57 +0900

    refactor: 파이썬 13주차 주석 오탈자 수정

--- week13/quiz/quiz1.py
+++ week13/quiz/quiz1.py
@@ -3,5 +3,5 @@ ss = 'IT_CookBook'
 print("1)", ss[0])           # 'I'
 print("2)", ss[1:2])         # 'T'
 print("3)", ss[3:])          # 'CookBook'
-print("4)", ss[:4])          # 'IT_Co'
+print("4)", ss[:4])          # 'IT_C'
 print("5)", ss[0:-1])        # 'IT_CookBoo' ← 마지막 문자 빠짐
