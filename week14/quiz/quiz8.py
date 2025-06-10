8a69c7b6beb250e1981816969d38694679322138
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-06-10 21:37:53 +0900

    refactor: 함수 인자 초기화 결과값 주석 표시

--- week14/quiz/quiz8.py
+++ week14/quiz/quiz8.py
@@ -2,7 +2,7 @@ def myFunc(p1=1, p2=2, p3=3):
     ret = p1 + p2 + p3
     return ret
 
-print("매개변수 없이 호출 ==> ", myFunc())
-print("매개변수가 1개로 호출 ==> ", myFunc(1))
-print("매개변수가 2개로 호출 ==> ", myFunc(1, 2))
-print("매개변수가 3개로 호출 ==> ", myFunc(1, 2, 3))
+print("매개변수 없이 호출 ==> ", myFunc())           # 6
+print("매개변수가 1개로 호출 ==> ", myFunc(1))       # 6
+print("매개변수가 2개로 호출 ==> ", myFunc(1, 2))    # 6
+print("매개변수가 3개로 호출 ==> ", myFunc(1, 2, 3)) # 6
