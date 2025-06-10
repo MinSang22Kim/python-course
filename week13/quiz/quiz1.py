2da0557fc99b263883ae5c08837b66eec39d07ae
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-06-10 19:15:45 +0900

    refactor: 인덱스 예시 추가

--- week13/quiz/quiz1.py
+++ week13/quiz/quiz1.py
@@ -5,3 +5,4 @@ print("2)", ss[1:2])         # 'T'
 print("3)", ss[3:])          # 'CookBook'
 print("4)", ss[:4])          # 'IT_C'
 print("5)", ss[0:-1])        # 'IT_CookBoo' ← 마지막 문자 빠짐
+print("6)", ss[0:-5])        # 'IT_Coo'
