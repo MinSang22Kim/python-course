3079f151efb0f8695cf421cdfb43016873f29fe5
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-06-10 18:50:29 +0900

    refactor: 주석 추가

--- week13/map_prac.py
+++ week13/map_prac.py
@@ -2,12 +2,12 @@
 str_nums = ['1', '2', '3']
 print(str_nums) # ['1', '2', '3']
 
-int_nums = list(map(int, str_nums))
+int_nums = list(map(int, str_nums)) # 각 문자열 요소에 int() 적용
 print(int_nums)  # [1, 2, 3]
 
 # 문자열 리스트를 대문자로 변환
 words = ['apple', 'banana', 'cherry']
-upper_words = list(map(str.upper, words))
+upper_words = list(map(str.upper, words)) # 각 문자열에 .upper() 적용
 print(upper_words)  # ['APPLE', 'BANANA', 'CHERRY']
 
 # 숫자 리스트의 각 값 제곱
