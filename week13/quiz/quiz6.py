4e8069be68261dbfd5f1286896ea376606bf5e0a
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-06-10 20:07:26 +0900

    refactor: 인덱스 슬라이드 코드 추가

--- week13/quiz/quiz6.py
+++ week13/quiz/quiz6.py
@@ -4,3 +4,16 @@ print("1) title():", ss.title())        # 각 단어 첫 글자 대문자
 print("2) upper():", ss.upper())        # 모두 대문자
 print("3) swapcase():", ss.swapcase())  # 대소문자 바꿔치기
 print("4) lower():", ss.lower())        # 모두 소문자
+
+
+s = "Python"
+for ch in s:
+    print(ch, end='-')
+
+s = "Python"
+print(s[::-1])
+
+new_str = ""
+ # 5 → 0, end값은 0이하로 작아져도(ex. -1, -5) 결과는 같음
+for i in range(len(s)-1, 0, -1):
+    new_str += s[i]
