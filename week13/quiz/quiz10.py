ad4b2363848f0295de872c67e75623b9e9cdc4c4
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-06-10 23:22:57 +0900

    refactor: 문자열 메서드 추가 설명

--- week13/quiz/quiz10.py
+++ week13/quiz/quiz10.py
@@ -1,4 +1,13 @@
 ss = "IT_CookBook"
-result = '#'.join(ss)
-print(result)
-# 출력: I#T#_#C#o#o#k#B#o#o#k
+
+result1 = '#'.join(ss)
+print(result1) # 출력: I#T#_#C#o#o#k#B#o#o#k
+
+result2 = ss.join('#')
+print(result2) # 출력: #
+
+result3 = ss.split('#')
+print(result3) # 출력: ['IT_CookBook']
+
+result4 = '#'.split(ss)
+print(result4) # 출력: ['#']
